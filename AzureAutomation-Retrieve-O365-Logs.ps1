1e295f24bf794f67ad18b621947c9b2732192cb3
Andy Routt <andy.routt@routt.org>
2022-12-11 22:50:05 -0600

    Initial Release

---
+++ AzureAutomation-Retrieve-O365-Logs.ps1
@@ -0,0 +1,284 @@
+<#
+.NOTES
+              Version:          1.0
+              Author:           Andy Routt
+              Creation Date:    12/10/2022
+              Purpose:          Azure Automation Runbook Script to Collect O365 Audit Logs and Write to an Azure Storage Blob
+              License:          MIT
+#> 
+
+#####################
+# USER VARIABLES
+#####################
+
+# Tenant Variables
+$tenantdomain = "[yourdomain].onmicrosoft.com"              # Tenant Domain
+$TenantGUID = "[tenant ID]"                                 # Tenant ID
+$Subscription = "[subscription ID]"                         # Azure Subscription ID
+
+# Runbook Variables
+$identitylName = "O365AuditLogsApp-Secret"                  # Azure Automation Account Credential Name
+$AppClientID = "[app registration ID]"                      # Applicatioin ID for Office Management API Registration
+
+# Storage Account Variables
+$resourceGroupName = "[resource group of storage account]"  # Storage Account Resource Group
+$storageAccountName = "[storage account name]"              # Storage Account Name
+$containerName = "[blob container name]"                    # Storage Account Container Name
+
+# Enable/Disable Debug Logging
+# $GLOBAL:DebugPreference="Continue"                          # Uncomment to Enable Debug
+
+Write-Debug "Debugging Enabled" 5>&1
+Write-Debug "" 5>&1ß
+
+
+#####################
+# SCRIPT VARIABLES
+#####################
+
+# OMI Variables
+$APIResource = "https://manage.office.com"
+$loginURL = "https://login.microsoftonline.com/"
+$BaseURI = "$APIResource/api/v1.0/$tenantGUID/activity/feed/subscriptions"
+$Subscriptions = @('Audit.AzureActiveDirectory','Audit.Exchange','Audit.SharePoint','Audit.General','DLP.All')
+$Date = Get-date
+$blobFolder = $Date.ToString('yyyy-MM-dd')
+$fileDate = $Date.ToString('yyyy-MM-dd_hh-mm-ss') + "Z"
+
+# SAS Variables
+$startTime     = Get-Date
+$expiryTime    = $startTime.AddDays(1)
+$permissions   = "rwl"
+$protocol      = "HttpsOnly"
+
+Write-Output "Date: $Date"
+Write-Output ""
+
+#####################
+# OAUTH TOKEN
+#####################
+
+# Retrieved Managed Identity Credential
+$myCredential = Get-AutomationPSCredential -Name $identitylName
+$securePassword = $myCredential.Password
+$ClientSecretValue = $myCredential.GetNetworkCredential().Password
+
+# Create OAUTH Token to access OMI Subscriptions Containing O365 Logs
+Write-Output "Obtaining OAUTH Token ..."
+$body = @{grant_type="client_credentials";resource=$APIResource;client_id=$AppClientID;client_secret=$ClientSecretValue}
+try {
+    $oauth = Invoke-RestMethod -Method Post -Uri "$loginURL/$tenantdomain/oauth2/token?api-version=1.0" -Body $body -ErrorAction Stop
+    $OfficeToken = @{'Authorization'="$($oauth.token_type) $($oauth.access_token)"}
+    Write-Output "SUCCESS"
+    Write-Output ""
+} catch {
+    Write-Output "FAILED"
+	Write-Output ""
+    Write-Output $error[0]
+    exit
+}
+
+# Debug
+$bearer_token = $OfficeToken.Authorization
+Write-Debug  "Bearer Token:" 5>&1
+Write-Debug  "" 5>&1
+Write-Debug  "$bearer_token" 5>&1
+Write-Debug  "" 5>&1
+
+$CheckSub = Invoke-WebRequest -Headers $OfficeToken -Uri "$BaseURI/list" -UseBasicParsing
+Write-Debug  "Subscriptions: ..." 5>&1
+Write-Debug  "" 5>&1
+Write-Debug  $CheckSub 3>&1
+Write-Debug  "" 5>&1
+
+#####################
+# SAS TOKEN
+#####################
+
+# Create Context to Hold Azure Authentication Session
+Write-Output "Connectiing to Azure ..."
+try {
+    Disable-AzContextAutosave -Scope Process | Out-Null
+    $AzureContext = (Connect-AzAccount -Identity).context
+    $AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
+	Write-Output "SUCCESS"
+	Write-Output ""
+} catch {
+    Write-Output "FAILED"
+	Write-Output ""
+    Write-Error -Message $_.Exception
+    throw $_.Exception
+    exit
+}
+
+# Create SAS Token for Writing to Azure Storage Blob
+Write-Output "Obtaining SAS Token ..."
+try {
+
+    # Create Azure Context to Access Azure Storage
+    $ctx = New-AzStorageContext -StorageAccountName $storageAccountName -UseConnectedAccount
+         
+    # Generate SAS Token for Blob Storage Container
+    $sas_token = New-AzStorageContainerSASToken `
+        -Context $ctx `
+        -Name $containerName `
+        -StartTime $startTime `
+        -ExpiryTime $expiryTime `
+        -Permission $permissions `
+        -Protocol $protocol
+    Write-Output "SUCCESS"
+    Write-Output ""
+
+} catch {
+    Write-Output "FAILED"
+    Write-Output ""
+    Write-Error -Message $_.Exception
+    throw $_.Exception
+    exit
+}
+
+# Debug
+Write-Debug  "SAS Token:" 5>&1
+Write-Debug  "" 5>&1
+Write-Debug  "$sas_token" 5>&1
+Write-Debug  "" 5>&1
+
+$existing_blobs = Get-AzStorageBlob -Container $containerName -Context $ctx | ft -property name, LastModified | Out-String
+Write-Debug "Existing Blobs: ..." 5>&1
+Write-Debug "" 5>&1
+Write-Debug "$existing_blobs" 5>&1
+Write-Debug "" 5>&1
+
+
+#####################
+# Functions
+#####################
+
+# Function -- Retrieve and Format Content URIs for Subscription
+function buildLog($BaseURI, $Subscription, $tenantGUID, $OfficeToken){
+
+    # Retrieve Content Page Containing URIs
+    try {
+        $Log = Invoke-WebRequest -Method GET -Headers $OfficeToken -Uri "$BaseURI/content?contentType=$Subscription&PublisherIdentifier=$TenantGUID" -UseBasicParsing -ErrorAction Stop
+    } catch {
+        Write-Output "Invoke-WebRequest ... FAILED"
+        Write-Output ""
+        Write-Output $error[0]
+        return
+    }
+
+    # Check for Additional Content Pages
+    if ($Log.Headers.NextPageUri) {
+        $NextContentPage = $true
+        $NextContentPageURI = $Log.Headers.NextPageUri
+
+        while ($NextContentPage -ne $false){
+
+            $ThisContentPage = Invoke-WebRequest -Headers $OfficeToken -Uri $NextContentPageURI -UseBasicParsing
+            $TotalContentPages += $ThisContentPage
+
+            if ($ThisContentPage.Headers.NextPageUri){
+                $NextContentPage = $true    
+            } Else {
+                $NextContentPage = $false
+            }
+            $NextContentPageURI = $ThisContentPage.Headers.NextPageUri
+        }
+    }
+    $TotalContentPages += $Log
+    return $TotalContentPages
+}
+
+# Function -- Format URIs and Export Logs to Azure Storage Blob
+function outputToFile($TotalContentPages, $JSONfilename, $Officetoken, $blobFolder, $containerName, $ctx){
+
+    # Retrieve URIs from Content Pages
+    if($TotalContentPages.content.length -gt 2){
+        $uris = @()
+        $pages = $TotalContentPages.content.split(",")
+        
+        foreach($page in $pages){
+            if($page -match "contenturi"){
+                $uri = $page.split(":")[2] -replace """"
+                $uri = "https:$uri"
+                $uris += $uri
+            }
+        }
+
+        # Fetch and Write All URIs
+        foreach($uri in $uris){
+            try {
+
+                # Debug
+                Write-Debug "Fetching ... $uri" 5>&1
+                Write-Debug  "" 5>&1
+
+                # Retrieve Log Data from Content URI
+                $Logdata += Invoke-RestMethod -Uri $uri -Headers $Officetoken -Method Get
+                
+                # Write Log Data to Azure Storage Blob Temp (Out-File Automatically Uses $env:temp)
+                $Logdata | ConvertTo-Json -Depth 100 | Out-File -Encoding UTF8 -FilePath $JSONfilename
+
+            } catch {
+                Write-Output "ERROR Fetching ... $uri"
+                Write-Output $error[0]
+                Write-Output ""
+                return
+            }      
+        }
+
+        # Store Blob Data by Date
+        $blobFileName = $blobFolder + "/" + $JSONfilename
+
+        # Retrieve Log Data from Azure Storage Blob Temp and Write to Azure Storage Blob Container
+        # There is a 1GB maximum file size of Azure Blob temporary sandbox storage
+        # https://learn.microsoft.com/en-us/azure/automation/automation-runbook-execution#temporary-storage-in-a-sandbox 
+        Write-Debug "Writing blob data to ... $blobFileName" 5>&1
+        Write-Debug  "" 5>&1
+        try {
+            Set-AzStorageBlobContent -File $JSONfilename -Blob $blobFileName -Container $containerName -Context $ctx | Out-null
+            Write-Output  "SAVED ... $blobFileName"
+
+        } catch {
+            Write-Output "ERROR Writing ... $blobFileName"
+            Write-Error -Message $_.Exception
+            throw $_.Exception
+            Write-Output ""
+        }
+
+    } else {
+
+        # Debug
+        Write-Debug  "" 5>&1
+        Write-Debug  "Moving to Next Subscription ..." 5>&1
+        Write-Debug  "" 5>&1
+    }
+}
+
+#####################
+# Export Logs
+#####################
+
+# Collect and Export Log Data by Subscription
+foreach($Subscription in $Subscriptions){
+    
+    # Debug
+    Write-Debug "" 5>&1
+    Write-Debug "Collecting log data from ..." 5>&1
+    Write-Debug "" 5>&1
+    Write-Debug "$Subscription" 5>&1
+    Write-Debug "" 5>&1
+
+    $logs = buildLog $BaseURI $Subscription $TenantGUID $OfficeToken
+    $JSONfilename = ($Subscription + "_" + $fileDate + ".json")
+
+    # DEBUG
+    Write-Debug "----- Content Pages -----" 5>&1
+    Write-Debug "$logs" 5>&1
+    Write-Debug "----- Content Pages -----" 5>&1
+    Write-Debug "" 5>&1
+
+    # Write JSON Log Data to Azure Blob Storage
+    outputToFile $logs $JSONfilename $OfficeToken $blobFolder $containerName $ctx
+
+}
---
+++ README.md
@@ -0,0 +1,159 @@
+# AzureAutomation-Retrieve-O365-Logs
+
+Azure Automation script to retrieve Office 365 Audit Logs. This script was adapted from the [Compliance-API Script](https://github.com/walidelmorsy/Microsoft-365-Compliance-API) to include the ability to execute as a Runbook and write the output to an Azure Storage Blob.
+
+
+### Prerequisites
+
+- Azure AD App Registration with Office Management API Permissions
+- Azure Automation Account
+- Azure Storage Account
+
+### Setup
+
+1. Create an Azure AD App Registration &rarr; [Register your application in Azure AD](https://learn.microsoft.com/en-us/office/office-365-management-api/get-started-with-office-365-management-apis#register-your-application-in-azure-ad)
+
+	- Follow the instructions to register a new Azure AD application.
+	<br>
+
+2. Create an App Registration Key &rarr; [Generate a new key for your application](https://learn.microsoft.com/en-us/office/office-365-management-api/get-started-with-office-365-management-apis#generate-a-new-key-for-your-application)
+
+	- Follow the instructions to create a new client secret to access the app registration.
+	<br>
+
+3. Assign Office Management API Permissions &rarr; [Specify the permissions your app requires to access the Office 365 Management APIs](https://learn.microsoft.com/en-us/office/office-365-management-api/get-started-with-office-365-management-apis#specify-the-permissions-your-app-requires-to-access-the-office-365-management-apis)
+
+	- Follow the instructions to grant Office Management API permissions to the App Registration.
+	<br>
+
+4. Grant Admin Consent for Office Management API Permissions &rarr; [Get Office 365 tenant admin consent](https://learn.microsoft.com/en-us/office/office-365-management-api/get-started-with-office-365-management-apis#get-office-365-tenant-admin-consent)
+
+	- Follow the instructions to grant Office Management API permissions.
+	<br>
+
+5. Generate an Access Token &rarr; [Request an access token by using client credentials](https://learn.microsoft.com/en-us/office/office-365-management-api/get-started-with-office-365-management-apis#request-an-access-token-by-using-client-credentials)
+
+	- Follow the instructions to generate and access token to receive Office 365 logs. 
+	<br>
+
+	<ins>BASH/ZSH Command Line Steps</ins>
+
+	```
+	% tenant="[your domain]"
+	% tenantid="[your tenant ID]"
+	% clientid="[your app registration ID]"
+	% clientsecret="[app registration client secret]"
+	% resource="https://manage.office.com"
+	
+	% token=`curl -s -X POST -H 'Content-Type: application/x-www-form-urlencoded' \
+	https://login.microsoftonline.com/$tenant/oauth2/token \
+	-d 'grant_type=client_credentials' \
+	-d client_id=$clientid \
+	-d client_secret=$clientsecret \
+	-d resource=$resource | jq -r '.access_token'`
+	```
+
+	<ins>PowerShell Steps</ins>
+
+	```
+	PS> $tenant="[your domain]"
+	PS> $tenantid="[your tenant ID]"
+	PS> $clientid="[your app registration ID]"
+	PS> $clientsecret="[app registration client secret]"
+	PS> $resource="https://manage.office.com" 
+	PS> $body=@{grant_type="client_credentials";resource=$resource;client_id=$clientid;client_secret=$clientsecret} 
+	PS> $oauth=Invoke-RestMethod -Method Post -Uri https://login.microsoftonline.com/$tenant/oauth2/token?api-version=1.0 -Body $body 
+	PS> $headerParams=@{'Authorization'="$($oauth.token_type) $($oauth.access_token)"}
+	```
+
+6. Start the Office Management API Subscriptions &rarr; [Start a subscription](https://learn.microsoft.com/en-us/office/office-365-management-api/office-365-management-activity-api-reference?source=recommendations#start-a-subscription)
+
+	- Follow the instructions to start a subscription for each content type. 
+	<br>
+
+
+	|        Content Type        |                  Description                  |
+	|----------------------------|-----------------------------------------------|
+	| Audit.AzureActiveDirectory | Azure Active Directory Logs (Office 365 Only) |
+	| Audit.Exchange             | Exchange Online Logs                          |
+	| Audit.SharePoint           | SharePoint Online Logs                        |
+	| Audit.General              | Other Logs                                    |
+	| DLP.All                    | DLP Logs                                      |
+
+
+	<ins>BASH/ZSH Command Line Steps</ins>
+
+	Start Subscriptions
+
+	```
+	% curl -s -d -G -X POST -H "Authorization: Bearer $token" https://manage.office.com/api/v1.0/$tenant/activity/feed/subscriptions/start?contentType=Audit.AzureActiveDirectory&PublisherIdentifier=$tenantid
+	% curl -s -d -G -X POST -H "Authorization: Bearer $token" https://manage.office.com/api/v1.0/$tenant/activity/feed/subscriptions/start?contentType=Audit.General&PublisherIdentifier=$tenantid
+	% curl -s -d -G -X POST -H "Authorization: Bearer $token" https://manage.office.com/api/v1.0/$tenant/activity/feed/subscriptions/start?contentType=Audit.Exchange&PublisherIdentifier=$tenantid
+	% curl -s -d -G -X POST -H "Authorization: Bearer $token" https://manage.office.com/api/v1.0/$tenant/activity/feed/subscriptions/start?contentType=Audit.SharePoint&PublisherIdentifier=$tenantid
+	% curl -s -d -G -X POST -H "Authorization: Bearer $token" https://manage.office.com/api/v1.0/$tenant/activity/feed/subscriptions/start?contentType=DLP.All&PublisherIdentifier=$tenantid
+	```
+	<ins>PowerShell Steps</ins>
+
+	Start Subscriptions
+
+	```
+	PS> Invoke-WebRequest -Method POST -Headers $headerParams -Uri "https://manage.office.com/api/v1.0/$tenant/activity/feed/subscriptions/start?contentType=Audit.AzureActiveDirectory&PublisherIdentifier=$tenantid"
+	PS> Invoke-WebRequest -Method POST -Headers $headerParams -Uri "https://manage.office.com/api/v1.0/$tenant/activity/feed/subscriptions/start?contentType=Audit.General&PublisherIdentifier=$tenantid"
+	PS> Invoke-WebRequest -Method POST -Headers $headerParams -Uri "https://manage.office.com/api/v1.0/$tenant/activity/feed/subscriptions/start?contentType=Audit.Exchange&PublisherIdentifier=$tenantid"
+	PS> Invoke-WebRequest -Method POST -Headers $headerParams -Uri "https://manage.office.com/api/v1.0/$tenant/activity/feed/subscriptions/start?contentType=Audit.SharePoint&PublisherIdentifier=$tenantid"
+	PS> Invoke-WebRequest -Method POST -Headers $headerParams -Uri "https://manage.office.com/api/v1.0/$tenant/activity/feed/subscriptions/start?contentType=DLP.All&PublisherIdentifier=$tenantid"
+	```
+
+7. Create an Azure Storage Account &rarr; [Create a storage account](https://learn.microsoft.com/en-us/azure/storage/common/storage-account-create?tabs=azure-portal#create-a-storage-account-1) 
+	- Follow the instructions to create a new storage account.
+	<br>
+
+8. Create a Blob Container &rarr; [Create a container](https://learn.microsoft.com/en-us/azure/storage/blobs/blob-containers-portal#create-a-container)
+	- Follow the instructions to create a new blob container within the storage account.
+	<br>
+
+9. Create an Azure Automation Account &rarr; [Create Automation account](https://learn.microsoft.com/en-us/azure/automation/quickstarts/create-azure-automation-account-portal#create-automation-account)
+
+	- Follow the instructions to create a new automation account.
+	<br>
+
+10. Configure Managed Identity &rarr; [Enable system-assigned managed identity](https://learn.microsoft.com/en-us/azure/automation/quickstarts/enable-managed-identity#enable-system-assigned-managed-identity)
+
+	Account Settings &rarr; Identity &rarr; System Assigned
+
+		- Status: On
+		- Permissions: Storage Blob Data Owner
+		- Resource: [storage account name]
+
+11. Add Client Secret to Access App Registration &rarr; [Create a new credential asset](https://learn.microsoft.com/en-us/azure/automation/shared-resources/credentials?tabs=azure-powershell#create-a-new-credential-asset)
+
+	Shared Resources &rarr; Credentials &rarr; Add a credential 
+
+		- Name: O365AuditLogsApp-Secret
+		- Password: [app registration client secret]
+
+12. Create Runbook  &rarr; [Create PowerShell runbook](https://learn.microsoft.com/en-us/azure/automation/learn/powershell-runbook-managed-identity#create-powershell-runbook)
+
+	Process Automation &rarr; Runbooks &rarr; Create a runbook &rarr; Edit &rarr; [Paste Script] &rarr; Save &rarr; Publish
+
+		- Name: Collect_O365_Logs
+		- Type: Powershell
+		- Version: 5.1
+
+13. Schedule Runbook &rarr; [Create a schedule](https://learn.microsoft.com/en-us/azure/automation/shared-resources/schedules#create-a-schedule)
+
+	Shared Resources &rarr; Schedules &rarr; Add a schedule
+
+		- Name: Collect O365 Daily
+		- Recurrence: Every 1 Day
+
+14. Link Schedule to Runbook &rarr; [Link a schedule to a runbook](https://learn.microsoft.com/en-us/azure/automation/shared-resources/schedules#link-a-schedule-to-a-runbook)
+
+
+	Process Automation &rarr; Runbooks &rarr; Collect_O365_Logs &rarr; Link to schedule &rarr; Link a schedule to your runbook &rarr; Collect O365 Logs Daily
+
+### Disclaimer
+
+This is a proof of concept script meant for testing purposes only. Use at your own risk.
+
+
